e14279cca959cde7dbc45223d59006bd8cdd56af
FerAcoFajardo <fernando.acosta216555@potros.itson.edu.mx>
2022-04-01 20:28:21 -0700

    Added strings to values

--- app/src/main/java/com/fipp/LoginActivity.kt
+++ app/src/main/java/com/fipp/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.fipp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
@@ -14,6 +15,14 @@ class LoginActivity : AppCompatActivity() {
 
         tvCreateAccount.setOnClickListener {
             val intent = Intent(this, CreateAccountActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Redirect to main activity
+        val btnLogin: Button = findViewById(R.id.button)
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
