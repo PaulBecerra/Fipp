6fe5624012c064e643dfea1d9292b9293444079b
FerAcoFajardo <fernando.acosta216555@potros.itson.edu.mx>
2022-04-01 19:01:29 -0700

    income and expenses started

---
+++ app/src/main/java/com/fipp/ExpensesStatisticsActivity.kt
@@ -0,0 +1,11 @@
+package com.fipp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExpensesStatisticsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_expenses_statistics)
+    }
+}
