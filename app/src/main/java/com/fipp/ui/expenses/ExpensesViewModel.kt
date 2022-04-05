36376760b7c30fe56fd52e66381bc1b9b4ff5516
FerAcoFajardo <fernando.acosta216555@potros.itson.edu.mx>
2022-04-05 01:37:59 -0700

    Some changes in expenses things

---
+++ app/src/main/java/com/fipp/ui/expenses/ExpensesViewModel.kt
@@ -0,0 +1,11 @@
+package com.fipp.ui.expenses
+
+import androidx.lifecycle.ViewModel
+
+class ExpensesViewModel : ViewModel() {
+
+//    private val _text = MutableLiveData<String>().apply {
+//        value = "This is home Fragment"
+//    }
+//    val text: LiveData<String> = _text
+}
