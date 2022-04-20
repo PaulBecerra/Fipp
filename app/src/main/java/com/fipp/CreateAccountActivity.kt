fac1d20618bb6298ecc468a0390204ee974580ae
PaulBecerra <paul.becerra216567@potros.itson.edu.mx>
2022-04-20 16:56:34 -0700

    major: added create account with email

--- app/src/main/java/com/fipp/CreateAccountActivity.kt
+++ app/src/main/java/com/fipp/CreateAccountActivity.kt
@@ -3,18 +3,79 @@ package com.fipp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.fipp.databinding.ActivityCreateAccountBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class CreateAccountActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCreateAccountBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_account)
 
-        val tvLogin: TextView = findViewById(R.id.textViewLogin)
+        binding = ActivityCreateAccountBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val tvLogin: TextView = binding.textViewLogin
 
         tvLogin.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+
+        auth = Firebase.auth
+
+        val btnEmail = binding.btnCreateAccountWithEmail
+        btnEmail.setOnClickListener{
+            val email = binding.editTextEmail.text.toString()
+            val pass = binding.editTextPassword.text.toString()
+            val passConfirm = binding.editTextConfirmPassword.text.toString()
+
+            if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                Toast.makeText(baseContext, "Email is not valid", Toast.LENGTH_SHORT).show()
+            } else if (pass.isEmpty()) {
+                Toast.makeText(baseContext, "Password is not valid", Toast.LENGTH_SHORT).show()
+            } else if (pass != passConfirm){
+                Toast.makeText(baseContext, "Passwords are not equals",  Toast.LENGTH_SHORT).show()
+            } else {
+                createAccount(email, pass)
+            }
+        }
+    }
+
+    private fun createAccount(email: String, password: String){
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("TAG", "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    if (user != null) {
+                        reload(user)
+                    }
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAG", "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun reload(user: FirebaseUser){
+        val intent = Intent(this, MainActivity::class.java).apply {
+            putExtra("email", user.email.toString())
+        }
+        startActivity(intent)
     }
 }
