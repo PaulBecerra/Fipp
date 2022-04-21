7ad5e4492c894eca6e1697f234fc014d15516349
PaulBecerra <paul.becerra216567@potros.itson.edu.mx>
2022-04-20 17:20:34 -0700

    major: added login with google

--- app/src/main/java/com/fipp/LoginActivity.kt
+++ app/src/main/java/com/fipp/LoginActivity.kt
@@ -10,8 +10,12 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.fipp.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
@@ -19,6 +23,8 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivityLoginBinding
+    // key for login with google
+    private val GOOGLE_SIGN_IN = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,6 +58,19 @@ class LoginActivity : AppCompatActivity() {
         if(currentUser != null){
             reload(currentUser);
         }
+
+        // google authenticator
+        val btnGoogle: Button = binding.btnGoogle
+
+        btnGoogle.setOnClickListener{
+            val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+            val googleClient = GoogleSignIn.getClient(this, googleConf)
+            googleClient.signOut()
+            startActivityForResult(googleClient.signInIntent, GOOGLE_SIGN_IN)
+        }
     }
 
     private fun login(email: String, password: String){
@@ -79,5 +98,42 @@ class LoginActivity : AppCompatActivity() {
             putExtra("email", user.email.toString())
         }
         startActivity(intent)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == GOOGLE_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try{
+                val account = task.getResult(ApiException::class.java)
+
+                if (account != null){
+                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+
+                    auth.signInWithCredential(credential).addOnCompleteListener{ it ->
+                        if (it.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithEmail:success")
+                            val user = auth.currentUser
+                            if (user != null) {
+                                reload(user)
+                            }
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithEmail:failure", it.exception)
+                            Toast.makeText(baseContext, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+                }
+            }catch (e: ApiException){
+                Toast.makeText(baseContext, "Authentication failed.",
+                    Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
