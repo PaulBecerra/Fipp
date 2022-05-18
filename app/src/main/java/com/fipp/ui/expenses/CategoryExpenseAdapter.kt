df70388df5e69fb31fb3bc5a97f9a9df02ba683d
PaulBecerra <paul.becerra216567@potros.itson.edu.mx>
2022-05-18 13:57:59 -0700

    feat: category image load from url

--- app/src/main/java/com/fipp/ui/expenses/CategoryExpenseAdapter.kt
+++ app/src/main/java/com/fipp/ui/expenses/CategoryExpenseAdapter.kt
@@ -12,11 +12,13 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.fipp.R
 import com.fipp.interfaces.ItemClickListener
 import com.fipp.model.Category
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
+import com.squareup.picasso.Picasso
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -24,10 +26,9 @@ import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 
 
-class CategoryExpenseAdapter(private var categoryList: List<Category>, var context: Context?) :
+class CategoryExpenseAdapter(private var categoryList: List<Category>, var activity: Activity?) :
     RecyclerView.Adapter<CategoryExpenseAdapter.ViewHolder>() {
     private var selectedPos = -1
-    private val storage = Firebase.storage
 
         class ViewHolder(view: View): RecyclerView.ViewHolder(view), View.OnClickListener{
             val image: ImageView
@@ -60,6 +61,15 @@ class CategoryExpenseAdapter(private var categoryList: List<Category>, var conte
         val category = categoryList[position]
         holder.categoryName.text = category.categoryName
         holder.subCategoryName.text = category.subCategory
+
+        val storageReference = Firebase.storage.reference
+
+        val pathReference = storageReference.child("img/${category.image}")
+
+        val url = "https://fipp-31ec2.appspot.com.storage.googleapis.com/img%2F${category.image}"
+        Glide.with(activity!! /* context */)
+            .load(url)
+            .into(holder.image)
 
         //holder.itemView.setSelected(selectedPos == position);
         holder.setOnClickListener (
