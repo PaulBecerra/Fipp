a066040599ed8041def9f04fb380761ee7335949
AlexaSanchez023 <85848696+AlexaSanchez023@users.noreply.github.com>
2022-05-18 01:41:27 -0700

    no editable email

--- app/src/main/java/com/fipp/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/fipp/ui/notifications/NotificationsFragment.kt
@@ -65,7 +65,7 @@ class NotificationsFragment : Fragment() {
         }
         btnGuardar.setOnClickListener{
 
-            userController.updateUser(nombre.toString(),correo.toString())
+            userController.updateUser(nombre.text.toString(),correo.text.toString())
 
         }
 
