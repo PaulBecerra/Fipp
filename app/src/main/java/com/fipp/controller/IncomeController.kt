5aaabe10183f36936c727eb606256e9ff5e79d13
FerAcoFajardo <fernando.acosta216555@potros.itson.edu.mx>
2022-05-17 21:14:12 -0700

    Create IncomeController

--- app/src/main/java/com/fipp/controller/IncomeController.kt
+++ app/src/main/java/com/fipp/controller/IncomeController.kt
@@ -5,15 +5,15 @@ import android.os.Build
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import com.fipp.model.Expense
+import com.fipp.model.Income
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.auth.User
 import com.google.firebase.ktx.Firebase
 import java.time.LocalDateTime
 import java.time.YearMonth
 
-class ExpenseController(private var activity: Activity) {
+class IncomeController(private var activity: Activity) {
     // key for login with google
     private val GOOGLE_SIGN_IN = 100
     private lateinit var auth: FirebaseAuth
@@ -24,15 +24,15 @@ class ExpenseController(private var activity: Activity) {
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    fun getMonthExpenses(month: Int, year: Int): ArrayList<Expense> {
-        val expenses = ArrayList<Expense>()
+    fun getMonthIncome(month: Int, year: Int): ArrayList<Income> {
+        val expenses = ArrayList<Income>()
         val user = auth.currentUser
         val userId = user?.uid
         // Create instnace of localDateTime with the month and year
         val yearMonthObject = YearMonth.of(year, month)
-        val daysInMonth = yearMonthObject.lengthOfMonth(); 
-        
-        
+        val daysInMonth = yearMonthObject.lengthOfMonth();
+
+
         val startDate = LocalDateTime.of(year, month, 1, 0, 0)
         val endDate = LocalDateTime.of(year, month, daysInMonth, 0, 0)
         db.collection("expenses").whereEqualTo("user", userId)
@@ -41,7 +41,7 @@ class ExpenseController(private var activity: Activity) {
             .get()
             .addOnSuccessListener {
                 for (document in it) {
-                    val expense = document.toObject(Expense::class.java)
+                    val expense = document.toObject(Income::class.java)
                     expenses.add(expense)
                     println(document.id + " => " + document.data)
                 }
@@ -53,5 +53,4 @@ class ExpenseController(private var activity: Activity) {
             }
         return expenses
     }
-
 }
